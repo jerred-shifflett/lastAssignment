c4dd81e1934cc4f894a175119fde7987ebd82526
jerred-shifflett <jerredshifflett@gmail.com>
2021-06-06 13:17:30 -0700

    fix linter errors

--- test_calculator.py
+++ test_calculator.py
@@ -16,7 +16,5 @@ class TestCalculatorApp:
     def test_multiply(self):
         assert 10 == calculator.multiply(2, 5)
 
-    def test_multiply(self):
+    def test_divide(self):
         assert 2 == calculator.divide(10, 5)
-
-
